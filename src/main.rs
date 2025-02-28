3142d9a2658121e0c5f92eef92e0d14f732a245f
Michael Bohn <spaceodyssey@gmx.de>
2025-02-28 08:34:48 +0100

    dependency update

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,22 @@ extern crate somag;
 use clap::{Arg, Command};
 use std::fs;
 
-use somag::z80;
 use somag::gameboy;
+use somag::z80;
 
 fn main() {
     let matches = Command::new("soma_g")
-                    .version("0.0.1")
-                    .about("gameboy emulator")
-                    .arg(Arg::new("ROMFILE")
-                               .help("ROM file input")
-                               .required(true)
-                               .index(1))
-                .get_matches();
+        .version("0.0.1")
+        .about("gameboy emulator")
+        .arg(
+            Arg::new("ROMFILE")
+                .help("ROM file input")
+                .required(true)
+                .index(1),
+        )
+        .get_matches();
 
-    let rom_file = matches.value_of("ROMFILE").unwrap();
+    let rom_file = matches.get_one::<String>("ROMFILE").unwrap();
     let rom = fs::read(rom_file).unwrap();
 
     let state = gameboy::gameboy_init(rom);
